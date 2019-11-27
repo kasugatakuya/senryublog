823af810f50941fec786f85584ea62302e997aa1
kasugatakuya <rxsgw924@mineo.jp>
2019-11-27 17:17:50 +0900

    非同期通信　setting

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,43 @@
+$(function(){
+  function buildMessage(message){
+    let html = `<div class="upper-message">
+                  <div class="lower-message">
+                    ${message.content}
+                  </div>
+                  <div class="upper-message__user-name">
+                    ${message.user_name}
+                  </div>
+                  <div class="upper-message__date">
+                    ${message.date}
+                  </div>
+                </div>`
+    return html;
+  }
+
+
+
+  $('#new_message').on('submit', function(e){   
+    e.preventDefault(); 
+    console.log(this)
+    let formData = new FormData(this); 
+    let url = $(this).attr('action');
+    $.ajax({ 
+      url: url,
+      type: "POST", 
+      data: formData, 
+      dataType: 'json', 
+      processData: false,
+      contentType: false 
+    })
+    .done(function(message){
+      console.log("成功");
+      let html = buildMessage(message);
+      $('.message').append(html)
+      $('#message_content').val('')
+    })
+    .fail(function(){
+      console.log("失敗");
+    })
+    return false;
+  })
+});
