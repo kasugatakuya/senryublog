561d2c2b86b0d0f01bdb6251779d3eaebf5824ae
kasugatakuya <rxsgw924@mineo.jp>
2020-01-06 22:59:10 +0900

    path-setting

---
+++ app/controllers/game_controller.rb
@@ -0,0 +1,13 @@
+class GameController < ApplicationController
+  def step1
+  end
+
+  def step2
+    @all_ranks = Post.find(Like.group(:post_id).order('count(post_id) desc').limit(5).pluck(:post_id))
+  end
+
+  def step3
+    @all_ranks = Text.find(Iine.group(:text_id).order('count(text_id) desc').limit(5).pluck(:text_id))
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -18,5 +18,15 @@ Rails.application.routes.draw do
   root to: "posts#index"
 
   resources :mercaris, only: [:index, :new, :create, :show, :edit]
+
+  resources :game do
+    collection do
+      get 'step1'
+      get 'step2'
+      get 'step3'
+      get 'step4' 
+      get 'done' 
+    end
+  end
   
 end
